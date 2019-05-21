d8a29cf11c116c4be7fae694fc422aa8da979ca3
Kristine McBride <kristinelmcbride@gmail.com>
2019-05-21 13:10:38 +0200

    booking controller

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :space
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :total_price, presence: true, numericality: { only_integer: false}
-  validates :status, presence: true, :in => ['pending', 'confirmed', 'declined']
+  validates :status, presence: true, in: ['pending', 'confirmed', 'declined']
 end
