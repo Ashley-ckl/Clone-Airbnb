ec8e944e66878a90acbdd36cc8ecb9093dd4b992
Kristine McBride <kristinelmcbride@gmail.com>
2019-05-21 11:43:00 +0200

    model_booking

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,3 +1,6 @@
 class Space < ApplicationRecord
   belongs_to :user
+  validates :capacity, presence: true, numericality: { only_integer: true}
+  validates :capacity, presence: true, numericality: { only_integer: false}
+  validates :price, presence: true
 end
