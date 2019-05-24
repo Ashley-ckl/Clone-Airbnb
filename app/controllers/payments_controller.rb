eaffbea95eaff0da43841f39b81e4867d14f1ee2
Meder Ashiraliev <mikeasher777@gmail.com>
2019-05-24 13:08:47 +0200

    time negative fixed

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,7 +9,7 @@ class PaymentsController < ApplicationController
 
   private
 
-def set_booking
+  def set_booking
     @booking = Booking.find(params[:booking_id])
   end
 end
--- app/policies/payment_policy.rb
+++ app/policies/payment_policy.rb
@@ -5,7 +5,7 @@ class PaymentPolicy < ApplicationPolicy
     end
   end
 
-   def new?
+  def new?
     true
   end
 
