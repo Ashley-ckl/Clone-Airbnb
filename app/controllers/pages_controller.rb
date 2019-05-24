cf0fc434f8a0423509e7b04a79458b0d4378788d
Kristine McBride <kristinelmcbride@gmail.com>
2019-05-24 10:07:10 +0200

    fixed margins and seeds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @home_spaces = Space.last(6)
+    @home_spaces = Space.last(8)
   end
 
 
